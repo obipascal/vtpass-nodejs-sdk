5c704d66851707e0ed126ffac5f97a3bbbb08bc0
Obi Pascal <pascalobi83@gmail.com>
2025-01-17 23:17:17 +0100

    updated types

--- src/types/responses.ts
+++ src/types/responses.ts
@@ -108,6 +108,23 @@ export interface VTPassMeterTokenTransactionResponse<T>
     fixChargeAmount: number | null;
     tariff: string;
     taxAmount: number | null;
+    MeterNumber: string;
+    Token: string;
+    ReceiptNumber: string | null;
+    PurchasedUnits: string;
+    DebtDescription: string | null;
+    DebtAmount: string | null;
+    RefundUnits: string | null;
+    ServiceChargeVatExcl: string | null;
+    Name: string;
+    Address: string;
+    Reference: string;
+    Vat: string | null;
+    ResponseTime: string | null;
+    TariffRate: string | null;
+    FreeUnits: string | null;
+    MeterCategory: string;
+    UtilityAmountVatExcl: string | null;
 }
 
 export interface SmartcardVerificationResponse {
